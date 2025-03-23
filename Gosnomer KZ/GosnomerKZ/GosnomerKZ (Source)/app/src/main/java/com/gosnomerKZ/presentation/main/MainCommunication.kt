79a7cd1c1f9d83138a5d30d01b0b5508724aa9c7
Meleor001 <abuch0677@gmail.com>
2025-03-23 22:44:00 +0300

    Delete Gosnomer KZ/GosnomerKZ directory

--- Gosnomer KZ/GosnomerKZ/GosnomerKZ (Source)/app/src/main/java/com/gosnomerKZ/presentation/main/MainCommunication.kt
+++
@@ -1,7 +0,0 @@
-package com.gosnomerKZ.presentation.main
-
-import com.gosnomerKZ.presentation.core.Communication
-
-interface MainCommunication: Communication<Pair<String, String>> {
-    class Base: Communication.Abstract<Pair<String, String>>(), MainCommunication
-}
--- Gosnomer KZ/GosnomerKZ/GosnomerKZ (Source)/app/src/main/java/com/gosnomerKZ/presentation/main/PriceCommunication.kt
+++
@@ -1,7 +0,0 @@
-package com.gosnomerKZ.presentation.main
-
-import com.gosnomerKZ.presentation.core.Communication
-
-interface PriceCommunication: Communication<String> {
-    class Base: Communication.Abstract<String>(), PriceCommunication
-}
